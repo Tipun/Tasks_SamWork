9e008a45b883275c77de2b53e2b4a19b53d8b632
MAKSIM <mtipun@mail.ru>
2021-09-24 15:37:14 +0300

    правка задачи

--- src/main/java/Praktika/t10/task03.java
+++ src/main/java/Praktika/t10/task03.java
@@ -6,8 +6,7 @@ import java.util.List;
 public class task03 {
     public static void main(String[] args) {
         int[]arr = {1,2,3,4,5,6,7};
-        int[]arr2 = {10,11,12,13,14,15,16};
-        int[]arr3 = new int [arr.length+arr2.length];
+        
 
         List<Integer>list = Arrays.asList(1,2,3,4,5,6,7);
         System.out.println(list);
