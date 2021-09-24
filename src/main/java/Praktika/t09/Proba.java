778873bf60926515f8183f348117d56456ddaebc
MAKSIM <mtipun@mail.ru>
2021-09-24 16:17:07 +0300

    изменение пробы

--- src/main/java/Praktika/t09/Proba.java
+++ src/main/java/Praktika/t09/Proba.java
@@ -4,6 +4,9 @@ public class Proba {
     public static void main(String[] args) {
         String text = "FIFA will never regret it";
         String[] words = text.split("\\s*(\\s|,|!|\\.)\\s*");
+        for (int i = 0; i <1000 ; i++) {
+            System.out.println(i);
+        }
         for(String word : words) {
             System.out.println(word);
         }
